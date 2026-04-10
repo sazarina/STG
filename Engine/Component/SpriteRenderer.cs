16dc3cec70a82c7739cd19f84252c59b58a05b14
Naoki Sato <sazarina100@gmail.com>
2026-04-10 14:10:43 +0900

    マージ

--- Engine/Component/SpriteRenderer.cs
+++ Engine/Component/SpriteRenderer.cs
@@ -1,17 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using STG.Engine.Graphics;
 
 namespace STG.Engine.Component {
-    public class SpriteRenderer :Component {
+    public class SpriteRenderer : Component {
+        #region Texture
+        public Texture2D texture { get; set; }
+        public LayerGroup SortingLayer { get; set; } = LayerGroup.Default;
+
+        public int SortingOrder { get; set; }
+
+
+        //使用するつもりはない。
+        void SetLayer(string layerName, int sortingOrder = 0) {
+            SortingLayer = new LayerGroup(layerName, sortingOrder, 0);
+            SortingOrder = sortingOrder;
+
+            //GameObjectManager.UpdateLayerGroup(gameObject, SortingLayer);
+        }
+
+        public Rectangle Rect {
+            get {
+                Rectangle rect = texture.Bounds;
+                rect.Location = transform.position.ToPoint();
+                return rect;
+            }
+        }
+        #endregion
         public override void Initialize() {
             base.Initialize();
         }
 
         public override void Update() {
             base.Update();
+        }
+
+        public void Draw() {
+            if (texture != null) {
+                GraphicsUltis.DrawSprite(texture, transform);
+            }
         }
 
     }
