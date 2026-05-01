30d984fe58b6d3f9922cb3833fdee68fbcce40d1
Naoki Sato <sazarina100@gmail.com>
2026-05-01 17:40:27 +0900

    画像の追加

--- STG/Bullet.cs
+++ STG/Bullet.cs
@@ -1,5 +1,7 @@
-﻿using STG.Engine.Component;
 using System;
+using Microsoft.Xna.Framework;
+using STG.Engine.Component;
+using STG.Engine.Graphics;
 
 namespace STG {
     class Bullet : Behavior {
@@ -12,20 +14,28 @@ namespace STG {
 
         float speed = 5f;
 
+        /// <summary>
+        /// ラジアン
+        /// </summary>
         float angle;
 
-        public void Shot(Type type, float angle) { 
+        public void Shot(Type type, float angle) {
             BulletType = type;
-            this.angle = angle;
+            this.angle = MathHelper.ToRadians(angle);
         }
 
         public override void Start() {
+            var sr = AddComponent<SpriteRenderer>();
 
+            sr.SortingLayer = Layer("Bullet");
+            sr.SortingOrder = 1;
 
             var spritesheet = new SpriteSheet("shot", 4, 1, new Point(12, 48));
+            sr.texture = spritesheet.GetTexture(1);
         }
 
         public override void Update() {
+            speed = (float)(new Random().NextDouble() * speed);
             var position = transform.position;
             position.X += (float)Math.Cos(angle) * speed;
             position.Y += (float)Math.Sin(angle) * speed;
